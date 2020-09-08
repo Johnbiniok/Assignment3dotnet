7fa69097d2e4a4b544d24c30d3b501838defb0d6
Johnbiniok <57683634+Johnbiniok@users.noreply.github.com>
2020-09-08 16:36:56 -0500

    update

--- WebApplicationtry2/wwwroot/js/balloons.js
+++ WebApplicationtry2/wwwroot/js/balloons.js
@@ -1,4 +1,33 @@
 $(function(){
     //$('#birthday').pickadate();
     $('#birthday').pickadate({ format: 'mmmm, d' });
+
+    // uncheck all checkboxes (FireFox)
+    $('.form-check-input').each(function () {
+        $(this).prop('checked', false);
+    });
+   // $(document).on('load', function(){
+        var allAnimations = ['bounce', 'flash', 'pulse', 'rubberBand', 'shakeX', 'shakeY', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'heartBeat'];
+        var animationToUse = Math.floor(Math.random() * 11);
+        $('#randomAnimate').replaceWith(`<h1 id="randomAnimate" class="animated ${allAnimations[animationToUse]} ">Happy Birthday!</h1>`);
+   //});
+    $('.form-check-input').on('change', function () {
+        // make the image visible
+        $('#' + this.id + 'Img').css('visibility', 'visible')
+        // animate balloon in/out based on checkbox
+        $(this).is(':checked') ?
+            $('#' + this.id + 'Img').removeClass().addClass('animated bounceInDown') :
+            $('#' + this.id + 'Img').addClass('animated bounceOutUp');
+    });
+    $( "#birthdayForm" ).on('submit',function(e) {
+        if ($("#birthdayForm input:checkbox:checked").length > 0)
+        {
+            // any one is checked
+        }
+        else
+        {
+            // TODO: create toast when submitted
+        }
+        e.preventDefault();
+    });
 });
--- WebApplicationtry2/wwwroot/js/client.js
+++ WebApplicationtry2/wwwroot/js/client.js
@@ -1,8 +1,13 @@
 $(function(){
     // preload audio
     var toast = new Audio('media/toast.wav');
+   /* var allAnimations = ['bounce', 'flash', 'pulse', 'rubberBand', 'shakeX', 'shakeY', 'headShake', 'swing', 'tada', 'wobble', 'jello', 'heartBeat'];
+    var animationToUse = Math.floor(Math.random() * 11);
+    $('#randomAnimate').replaceWith(`'<h1 id="randomAnimate" class="animated ${allAnimations[animationToUse]} ">Happy Birthday!</h1><hr />'`);*/
+
 
     $('.code').on('click', function(e) {
+
         e.preventDefault();
         // first pause the audio (in case it is still playing)
         toast.pause();
@@ -10,7 +15,9 @@ $(function(){
         toast.currentTime = 0;
         // play audio
         toast.play();
+        $('#code').data('data-code');
         $('#toast').toast({ autohide: false }).toast('show');
+
     });
 });
 $(document).on('keyup', function (e) {
